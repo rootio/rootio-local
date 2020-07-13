d228a987d6ab3cde694067600800c335d7932336
Jude Mukundane <jude19love@gmail.com>
2020-07-13 16:54:41 +0100

    Added test file for DBAgent class

---
+++ src/test/java/org/rootio/tools/persistence/DBAgentTest.java
@@ -0,0 +1,34 @@
+package org.rootio.tools.persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBAgentTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteRecords() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateRecords() {
+    }
+}
