6917765e988798e7f4d467aa542258cf1048c505
Jude Love <jude17love@hotmail.com>
2020-07-15 21:33:39 +0100

    Adapted CallLogHandler.java to pick logs from SQLITE

--- src/main/java/org/rootio/services/synchronization/CallLogHandler.java
+++ src/main/java/org/rootio/services/synchronization/CallLogHandler.java
@@ -1,39 +1,23 @@
 package org.rootio.services.synchronization;
 
-import java.util.Calendar;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.rootio.handset.BuildConfig;
-import org.rootio.handset.R;
-import org.rootio.tools.cloud.Cloud;
+import org.rootio.configuration.Configuration;
+import org.rootio.tools.persistence.DBAgent;
 import org.rootio.tools.utils.Utils;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.CallLog;
-import android.util.Log;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CallLogHandler implements SynchronizationHandler {
 
-    private Context parent;
-    private ContentResolver cr;
-    private Uri uri;
-    private Cloud cloud;
 
-    CallLogHandler(Context parent, Cloud cloud) {
-        this.parent = parent;
-        this.cloud = cloud;
-        this.prepareContentResolver();
-    }
-
-    private void prepareContentResolver() {
-        cr = this.parent.getContentResolver();
-        uri = CallLog.Calls.CONTENT_URI;
+    CallLogHandler() {
 
     }
 
@@ -41,34 +25,36 @@ public class CallLogHandler implements SynchronizationHandler {
     public JSONObject getSynchronizationData() {
         JSONObject data = new JSONObject();
         JSONArray calls = new JSONArray();
-        String sortOrder = CallLog.Calls.DATE + " ASC";
-        String[] columns = new String[]{CallLog.Calls._ID, CallLog.Calls.NUMBER, CallLog.Calls.DURATION, CallLog.Calls.DATE, CallLog.Calls.TYPE};
-        String filter = "_id > ?";
-        String[] args = new String[]{String.valueOf(this.getMaxId())};
+        List<String> sortOrder =  Arrays.asList("call_date");
+        String sortDirection = "asc";
+        List<String> columns = Arrays.asList("id", "b_party", "call_duration", "call_date", "call_type");
+        List<String> filterColumns = Arrays.asList("id");
+        List<String> args = Arrays.asList(String.valueOf(this.getMaxId()));
         try {
-            Cursor cur = cr.query(uri, columns, filter, args, sortOrder);
-            if (cur != null && cur.getCount() > 0) {
-                while (cur.moveToNext()) {
+            List<List<Object>> results = DBAgent.getData("calls",columns,filterColumns,args,null, null,sortOrder, sortDirection, null);
+            results.forEach( record -> {
                     JSONObject callRecord = new JSONObject();
-                    callRecord.put("call_uuid", cur.getLong(0));
-                    if (cur.getInt(4) == CallLog.Calls.INCOMING_TYPE || cur.getInt(4) == CallLog.Calls.MISSED_TYPE) {
-                        callRecord.put("from_phonenumber", cur.getString(1));
+                try {
+                    callRecord.put("call_uuid", (long)record.get(0));
+                if ((String)record.get(4) == "incoming" || (String)record.get(4) == "missed") {
+                        callRecord.put("from_phonenumber", (String)record.get(1));
                         callRecord.put("to_phonenumber", "");
                     } else {
                         callRecord.put("from_phonenumber", "");
-                        callRecord.put("to_phonenumber", cur.getString(1));
+                        callRecord.put("to_phonenumber", (String)record.get(1));
                     }
-                    callRecord.put("duration", cur.getString(2));
+                    callRecord.put("duration", (int)record.get(2));
                     Calendar cal = Calendar.getInstance();
-                    cal.setTimeInMillis(cur.getLong(3));
+                    cal.setTimeInMillis((long)record.get(3));
                     callRecord.put("start_time", Utils.getDateString(cal.getTime(), "yyyy-MM-dd HH:mm:ss"));
                     calls.put(callRecord);
+                } catch (JSONException e) {
+                    Logger.getLogger("org.rootio").log(Level.WARNING, "CallLogHandler.getSynchronizationData: " + e.getMessage());
                 }
-            }
-            cur.close();
+                });
             data.put("call_data", calls);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (JSONException | SQLException e) {
+            Logger.getLogger("org.rootio").log(Level.WARNING, "CallLogHandler.getSynchronizationData: " + e.getMessage());
         }
         return data;
     }
@@ -93,17 +79,21 @@ public class CallLogHandler implements SynchronizationHandler {
     }
 
     private void logLastId(long id) {
-        ContentValues values = new ContentValues();
-        values.put("call_id", id);
-        Utils.savePreferences(values, this.parent);
+        Configuration.setProperty("call_id", String.valueOf(id));
     }
 
     private long getMaxId() {
-        return (long)Utils.getPreference("call_id", long.class, this.parent);
+        try {
+            return Long.parseLong(Configuration.getProperty("call_id"));
+        }
+        catch (NumberFormatException ex)
+        {
+            throw ex;
+        }
     }
 
     @Override
     public String getSynchronizationURL() {
-        return String.format("%s://%s:%s/%s/%s/call?api_key=%s&version=%s_%s", this.cloud.getServerScheme(), this.cloud.getServerAddress(), this.cloud.getHTTPPort(), "api/station", this.cloud.getStationId(), this.cloud.getServerKey(), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
+        return String.format("%s://%s:%s/%s/%s/call?api_key=%s&version=%s_%s", Configuration.getProperty("server_scheme"), Configuration.getProperty("server_address"), Configuration.getProperty("port"), "api/station", Configuration.getProperty("station_id"), Configuration.getProperty("Station_key"), Configuration.getProperty("version_name"), Configuration.getProperty("version_code"));
     }
 }
