972048925a474bc48ea6fa85710c1ec947ee898c
Jude Love <jude17love@hotmail.com>
2020-07-23 18:25:44 +0100

    Adapted the PlaylisstHandler class

--- src/main/java/org/rootio/services/synchronization/PlaylistHandler.java
+++ src/main/java/org/rootio/services/synchronization/PlaylistHandler.java
@@ -1,53 +1,42 @@
-/**
- *
- */
 package org.rootio.services.synchronization;
 
-import java.util.ArrayList;
-
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-import org.rootio.handset.BuildConfig;
-import org.rootio.handset.R;
-import org.rootio.tools.cloud.Cloud;
+import org.rootio.configuration.Configuration;
 import org.rootio.tools.persistence.DBAgent;
 
-import android.content.Context;
-import android.util.Log;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Jude Mukundane, M-ITI/IST-UL
  */
 public class PlaylistHandler implements SynchronizationHandler {
 
-    private Context parent;
-    private Cloud cloud;
 
-    PlaylistHandler(Context parent, Cloud cloud) {
-        this.parent = parent;
-        this.cloud = cloud;
+    PlaylistHandler() {
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.rootio.services.synchronization.SynchronizationHandler#
-     * getSynchronizationData()
-     */
+
     @Override
     public JSONObject getSynchronizationData() {
         return new JSONObject();
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.rootio.services.synchronization.SynchronizationHandler#
-     * processJSONResponse(org.json.JSONObject)
-     */
+
     @Override
     public void processJSONResponse(JSONObject synchronizationResponse) {
-        ArrayList<String[]> playlistItems = new ArrayList<String[]>();
+        try {
+            DBAgent.deleteRecords("playlist", null, null);//empty playlist info
+        } catch (SQLException e) {
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[PlaylistHandler.processJSONObject]" : e.getMessage());
+            return;
+        }
         try {
             JSONArray playlists = synchronizationResponse.getJSONArray("objects");
             for (int i = 0; i < playlists.length(); i++) {
@@ -55,36 +44,41 @@ public class PlaylistHandler implements SynchronizationHandler {
                 // Save the songs
                 JSONArray songs = playlist.getJSONArray("songs");
                 for (int j = 0; j < songs.length(); j++) {
-                    playlistItems.add(new String[]{playlist.getString("title"), songs.getJSONObject(j).getString("title"), "1"});
+                    savePlaylistItem(Arrays.asList(playlist.getString("title"), songs.getJSONObject(j).getString("title")), "1");
                 }
                 // save the Albums
                 JSONArray albums = playlist.getJSONArray("albums");
                 for (int j = 0; j < albums.length(); j++) {
-                    playlistItems.add(new String[]{playlist.getString("title"), albums.getJSONObject(j).getString("title"), "2"});
+                    savePlaylistItem(Arrays.asList(playlist.getString("title"), songs.getJSONObject(j).getString("title")), "2");
                 }
                 // save the Artists
                 JSONArray artists = playlist.getJSONArray("artists");
                 for (int j = 0; j < artists.length(); j++) {
-                    playlistItems.add(new String[]{playlist.getString("title"), artists.getJSONObject(j).getString("title"), "3"});
+                    savePlaylistItem(Arrays.asList(playlist.getString("title"), songs.getJSONObject(j).getString("title")), "3");
                 }
             }
-           // DBAgent dbAgent = new DBAgent(this.parent);
-            DBAgent.deleteRecords("playlist", null, null);//empty playlist info
-            DBAgent.bulkSaveData("playlist", null, new String[]{"title", "item", "itemtypeid"}, playlistItems.toArray(new String[playlistItems.size()][]));
-        } catch (Exception ex) {
-            Log.e(this.parent.getString(R.string.app_name), ex.getMessage() == null ? "Null pointer exception(PlaylistHandler.processJSONResponse)" : ex.getMessage());
+        } catch (JSONException e) {
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[PlaylistHandler.processJSONObject]" : e.getMessage());
         }
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.rootio.services.synchronization.SynchronizationHandler#
-     * getSynchronizationURL()
-     */
+    private long savePlaylistItem(List<String> values, String type)
+    {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("title", values.get(0));
+        data.put("item", values.get(1));
+        data.put("itemtypeid", type);
+        try {
+            DBAgent.saveData("playlist", data);
+        } catch (SQLException e) {
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[PlaylistHandler.processJSONObject]" : e.getMessage());
+        }
+        return -1;
+    }
+
     @Override
     public String getSynchronizationURL() {
-        return String.format("%s://%s:%s/%s/%s/playlists?api_key=%s&version=%s_%s", this.cloud.getServerScheme(), this.cloud.getServerAddress(), this.cloud.getHTTPPort(), "api/station", this.cloud.getStationId(), this.cloud.getServerKey(), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
+        return String.format("%s://%s:%s/%s/%s/playlists?api_key=%s&version=%s_%s", Configuration.getProperty("server_scheme"), Configuration.getProperty("server_address"), Configuration.getProperty("http_port"), "api/station", Configuration.getProperty("station_id"), Configuration.getProperty("server_key"), Configuration.getProperty("build_version"), Configuration.getProperty("build_version"));
     }
 
 }
