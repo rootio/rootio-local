76bceb354acc35052b9106dfe540f71f71fea622
Jude Love <jude17love@hotmail.com>
2020-07-23 15:27:38 +0100

    Adapted the Utils class

--- src/main/java/org/rootio/tools/utils/Utils.java
+++ src/main/java/org/rootio/tools/utils/Utils.java
@@ -1,13 +1,8 @@
 package org.rootio.tools.utils;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.util.Log;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.rootio.configuration.Configuration;
-import org.rootio.handset.R;
-import org.rootio.messaging.Message;
 import org.rootio.tools.persistence.DBAgent;
 
 import java.io.*;
@@ -18,27 +13,19 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Utils {
 
 
     public static Long getEventTimeId(long programId, Date scheduleDate, int duration) throws SQLException {
-        String tableName = "eventtime";
-        List<String> columns = Arrays.asList("id");
-        List<String> whereClause = Arrays.asList("programid", "duration", "scheduledate");
+        String query = "select id from eventtime where program_id = ? and duration = ? and schedule_date = ?";
         List<String> whereArgs = Arrays.asList(String.valueOf(programId), String.valueOf(duration), Utils.getDateString(scheduleDate, "yyyy-MM-dd HH:mm:ss"));
-        //DBAgent dbAgent = new DBAgent(parent);
-        List<List<Object>> results = DBAgent.getData(tableName, columns, whereClause, whereArgs, null, null, null, null);
-        return results.size() > 0 ? (Long)results.get(0).get(0) : 0l;
+        List<List<Object>> results = DBAgent.getData(query, whereArgs);
+        return results.size() > 0 ? (Long)results.get(0).get(0) : 0L;
     }
-
-    public static void toastOnScreen(Message m) {
-        //Throw a message to the logger via rabbit MQ or similar
-    }
-
 
     public static String getCurrentDateAsString(String format) {
         SimpleDateFormat sdf = new SimpleDateFormat(format);
@@ -92,69 +79,6 @@ public class Utils {
         }
     }
 
-    public static InetAddress parseInetAddressFromString(String input) {
-        try {
-            InetAddress address = InetAddress.getByName(input);
-            return address;
-        } catch (Exception ex) {
-            return null;
-        }
-    }
-
-    public static JSONObject getJSONFromFile(String fileName) throws IOException {
-        FileInputStream input = null;
-        try {
-            File jsonFile = new File(fileName);
-            input = new FileInputStream(jsonFile);
-            byte[] buffer = new byte[1024];
-            input.read(buffer);
-            return new JSONObject(new String(buffer));
-        } catch (Exception ex) {
-            Logger.getLogger("org.rootio").log(Level.SEVERE, (ex.getMessage() == null) ? "NullPointerException(CallAuthenticator.isWhiteListed)" : ex.getMessage());
-            throw ex;
-        } finally {
-            try {
-                input.close();
-            } catch (Exception ex) {
-                // log the exception
-            }
-        }
-
-    }
-    }
-
-    public static void savePreferences(HashMap<String, String> values) {
-        values.forEach((k,v) -> Configuration.setProperty(k, v));
-    }
-
-    public static Object getPreference(String key, Class cls, Context context)
-    {
-        try{
-        SharedPreferences prefs = context.getSharedPreferences("org.rootio.handset", Context.MODE_PRIVATE);
-        if(prefs != null) {
-            if (cls == String.class) {
-                return prefs.getString(key, null);
-            } else if (cls == int.class) {
-                return prefs.getInt(key, 0);
-            } else if (cls == boolean.class) {
-                return prefs.getBoolean(key, false);
-            } else if (cls == long.class) {
-                return prefs.getLong(key, 0l);
-            } else if (cls == float.class) {
-                return prefs.getFloat(key, 0f);
-            }
-        }}
-        catch(Exception ex)
-        {
-            ex.printStackTrace();
-        }
-        catch (StackOverflowError er)
-        {
-            er.printStackTrace();
-        }
-        return null;
-    }
-
     public static String doPostHTTP(String httpUrl, String data) {
         URL url;
         try {
@@ -178,9 +102,6 @@ public class Utils {
                 response.append((char) tmp);
             }
             return response.toString();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return null;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -189,7 +110,7 @@ public class Utils {
 
     public static HashMap<String, Object> doDetailedPostHTTP(String httpUrl, String data) {
         URL url;
-        Long then  = Calendar.getInstance().getTimeInMillis();
+        long then  = Calendar.getInstance().getTimeInMillis();
         HashMap<String, Object> responseData = new HashMap<>();
         try {
             url = new URL(httpUrl);
@@ -217,9 +138,6 @@ public class Utils {
             responseData.put("length", httpUrlConnection.getContentLength());
             responseData.put("url", httpUrlConnection.getURL());
             return responseData;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return null;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -231,20 +149,19 @@ public class Utils {
      *
      * @return True if connected, false if not connected
      */
-    public static boolean isConnectedToStation(Context context) {
-        SharedPreferences prefs = context.getSharedPreferences("org.rootio.handset", Context.MODE_PRIVATE);
-        return prefs != null && prefs.contains("station_information");
+    public static boolean isConnectedToStation() {
+        return Configuration.isSet("station_name");
      }
 
-     public static long logEvent(Context context, EventCategory category, EventAction action, String argument)
+     public static long logEvent(EventCategory category, EventAction action, String argument)
      {
           try {
-             ContentValues values = new ContentValues();
+             HashMap<String, Object> values = new HashMap<>();
              values.put("category", category.name());
              values.put("argument", argument);
              values.put("event", action.name());
              values.put("eventdate", Utils.getCurrentDateAsString("yyyy-MM-dd HH:mm:ss"));
-             return DBAgent.saveData("activitylog", null, values);
+             return DBAgent.saveData("activitylog", values);
          }
          catch (Exception ex)
          {
@@ -253,13 +170,5 @@ public class Utils {
          return 0;
      }
 
-    public enum EventCategory
-    {
-        MEDIA, SERVICES, SYNC, SMS, CALL, SIP_CALL, DATA_NETWORK
-    }
 
-    public enum EventAction
-    {
-        ON, OFF, PAUSE, STOP, START, SEND, RECEIVE, REGISTRATION, RINGING
-     }
 }
