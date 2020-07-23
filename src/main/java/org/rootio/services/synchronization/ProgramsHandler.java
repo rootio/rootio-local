c42abddec75bb6a0302fa1abf197efef24aaffbd
Jude Love <jude17love@hotmail.com>
2020-07-23 19:43:30 +0100

    Adapted ProgramHandler class

--- src/main/java/org/rootio/services/synchronization/ProgramsHandler.java
+++ src/main/java/org/rootio/services/synchronization/ProgramsHandler.java
@@ -1,32 +1,25 @@
 package org.rootio.services.synchronization;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.support.annotation.NonNull;
-
+import jdk.jshell.spi.ExecutionControl;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.rootio.handset.BuildConfig;
-import org.rootio.tools.cloud.Cloud;
+import org.rootio.configuration.Configuration;
 import org.rootio.tools.persistence.DBAgent;
 import org.rootio.tools.utils.Utils;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProgramsHandler implements SynchronizationHandler {
 
-    private Context parent;
-    private Cloud cloud;
-    private int records=2000;
+   private int records=2000;
 
-    ProgramsHandler(Context parent, Cloud cloud) {
-        this.parent = parent;
-        this.cloud = cloud;
+    ProgramsHandler() {
     }
 
     @Override
@@ -48,7 +41,7 @@ public class ProgramsHandler implements SynchronizationHandler {
         boolean hasChanges = false;
         boolean shouldRestart = false;
         boolean result = false;
-        ArrayList<ContentValues> values = new ArrayList();
+        ArrayList<HashMap<String, Object>> values = new ArrayList<>();
         //long result = 0;
         JSONArray results;
         try {
@@ -56,7 +49,7 @@ public class ProgramsHandler implements SynchronizationHandler {
 
             for (int i = 0; i < results.length(); i++) {
                 //if the record did not exist in the DB, then it is new ;-)
-                int recs = this.deleteRecord(results.getJSONObject(i).getLong("scheduled_program_id"));
+                long recs = this.deleteRecord(results.getJSONObject(i).getLong("scheduled_program_id"));
                 if ((recs < 1 || results.getJSONObject(i).getBoolean("deleted")) && (isCurrentOrFutureChange(results.getJSONObject(i).getString("start"), results.getJSONObject(i).getString("end")))) {
                     hasChanges = true;
                     if(results.getJSONObject(i).getInt("program_type_id") == 2 && isCurrent(results.getJSONObject(i).getString("start"), results.getJSONObject(i).getString("end")))
@@ -68,32 +61,25 @@ public class ProgramsHandler implements SynchronizationHandler {
                 values.add(getContentValues(results.getJSONObject(i).getInt("scheduled_program_id"), results.getJSONObject(i).getString("name"), Utils.getDateFromString(results.getJSONObject(i).getString("start"), "yyyy-MM-dd'T'HH:mm:ss"), Utils.getDateFromString(results.getJSONObject(i).getString("end"), "yyyy-MM-dd'T'HH:mm:ss"), results.getJSONObject(i).getString("structure"), Utils.getDateFromString(results.getJSONObject(i).getString("updated_at"), "yyyy-MM-dd'T'HH:mm:ss"), results.getJSONObject(i).getString("program_type_id"), results.getJSONObject(i).getBoolean("deleted")));
             }
             if(values.size() > 0) {
-                result = this.saveRecords(values);
+                result = this.saveRecords(values) > 0;
             }
             if (result && hasChanges) {
                 this.announceScheduleChange(shouldRestart);
             }
-            if(results.length() == this.records) // we had a full page, maybe more records..
-            {
-                 this.requestSync(true);
-            }
-            else {
-                this.requestSync(false);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
+            // we had a full page, maybe more records..
+            this.requestSync(results.length() == this.records);
+        } catch (ExecutionControl.NotImplementedException | JSONException e) {
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[ProgramHandler.processJSONObject]" : e.getMessage());
         }
     }
 
-    private void requestSync(boolean isStarting) {
-        Intent intent = new Intent();
-        intent.setAction("org.rootio.services.synchronization.SYNC_REQUEST");
-        intent.putExtra("category", 2);
-        intent.putExtra("sync", isStarting?"start": "end");
-        this.parent.sendBroadcast(intent);
+    private void requestSync(boolean isStarting) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Not yet implemented");
+//        Intent intent = new Intent();
+//        intent.setAction("org.rootio.services.synchronization.SYNC_REQUEST");
+//        intent.putExtra("category", 2);
+//        intent.putExtra("sync", isStarting?"start": "end");
+//        this.parent.sendBroadcast(intent);
     }
 
     private boolean isCurrent(String startDateStr, String endDateStr)
@@ -113,44 +99,58 @@ public class ProgramsHandler implements SynchronizationHandler {
             Date now = Calendar.getInstance().getTime();
             Date startDate = Utils.getDateFromString(startDateStr, "yyyy-MM-dd'T'HH:mm:ss");
             Date endDate = Utils.getDateFromString(endDateStr, "yyyy-MM-dd'T'HH:mm:ss");
-            Boolean isToday = now.getYear() == startDate.getYear() && now.getMonth() == startDate.getMonth() && now.getDate() == startDate.getDate();
+            boolean isToday = now.getYear() == startDate.getYear() && now.getMonth() == startDate.getMonth() && now.getDate() == startDate.getDate();
             return (isToday && startDate.compareTo(now) >= 0) || (startDate.compareTo(now) <= 0 && endDate.compareTo(now) >= 0); //yet to begin today, or is running
         } catch (Exception ex) {
             return false;
         }
     }
 
-    private void announceScheduleChange(boolean shouldRestart) {
-        Intent intent = new Intent("org.rootio.services.synchronization.SCHEDULE_CHANGE_EVENT");
-        intent.putExtra("shouldRestart", shouldRestart);
-        this.parent.sendBroadcast(intent);
+    private void announceScheduleChange(boolean shouldRestart) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Yet to be implemented");
+//        Intent intent = new Intent("org.rootio.services.synchronization.SCHEDULE_CHANGE_EVENT");
+//        intent.putExtra("shouldRestart", shouldRestart);
+//        this.parent.sendBroadcast(intent);
     }
 
     @Override
     public String getSynchronizationURL() {
-        return String.format("%s://%s:%s/%s/%s/programs?api_key=%s&%s&version=%s_%s", this.cloud.getServerScheme(), this.cloud.getServerAddress(), this.cloud.getHTTPPort(), "api/station", this.cloud.getStationId(), this.cloud.getServerKey(), this.getSincePart(), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
+        return String.format("%s://%s:%s/%s/%s/programs?api_key=%s&version=%s_%s", Configuration.getProperty("server_scheme"), Configuration.getProperty("server_address"), Configuration.getProperty("http_port"), "api/station", Configuration.getProperty("station_id"), Configuration.getProperty("server_key"), Configuration.getProperty("build_version"), Configuration.getProperty("build_version"));
     }
 
     private String getSincePart() {
         String query = "select max(updatedat) from scheduledprogram";
-        String[][] result = DBAgent.getData(query, new String[]{});
-        if (result == null || result.length == 0 || result[0][0] == null) {
-            return String.format("all=1&records=%s", this.records); //Todo: implement start and end based filtering to not fetch very old records
+        List<List<Object>> result;
+        try {
+            result = DBAgent.getData(query, Collections.emptyList());
+            if (!result.isEmpty()) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(Utils.getDateFromString((String)result.get(0).get(0), "yyyy-MM-dd HH:mm:ss"));
+                cal.add(Calendar.SECOND, 1); //Add 1 second, server side compares using greater or equal
+                return String.format("updated_since=%s&records=%s", Utils.getDateString(cal.getTime(), "yyyy-MM-dd'T'HH:mm:ss"), records);
+            }
+        } catch (NullPointerException | SQLException e) {
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[ProgramHandler.getSynchronizationHandler]" : e.getMessage());
         }
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(Utils.getDateFromString(result[0][0], "yyyy-MM-dd HH:mm:ss"));
-        cal.add(Calendar.SECOND, 1); //Add 1 second, server side compares using greater or equal
-        return String.format("updated_since=%s&records=%s", Utils.getDateString(cal.getTime(), "yyyy-MM-dd'T'HH:mm:ss"), records);
+        return String.format("all=1&records=%s", this.records);
     }
 
-    private boolean saveRecords(ArrayList<ContentValues> values) {
+    private long saveRecords(ArrayList<HashMap<String, Object>> values) {
         String tableName = "scheduledprogram";
-        return DBAgent.bulkSaveData(tableName, null, values.toArray(new ContentValues[values.size()]));
+        AtomicLong result = new AtomicLong(0);
+
+            values.forEach(row -> {
+                try {
+                result.addAndGet(DBAgent.saveData(tableName, row));
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            });
+        return result.get();
     }
 
-    @NonNull
-    private ContentValues getContentValues(int id, String name, Date start, Date end, String structure, Date updatedAt, String programTypeId, Boolean deleted) {
-        ContentValues data = new ContentValues();
+    private HashMap<String, Object> getContentValues(int id, String name, Date start, Date end, String structure, Date updatedAt, String programTypeId, Boolean deleted) {
+        HashMap<String, Object> data = new HashMap();
         data.put("id", id);
         data.put("name", name);
         data.put("start", Utils.getDateString(start, "yyyy-MM-dd HH:mm:ss"));
@@ -162,11 +162,15 @@ public class ProgramsHandler implements SynchronizationHandler {
         return data;
     }
 
-    private int deleteRecord(long id) {
+    private long deleteRecord(long id) {
         String tableName = "scheduledprogram";
         String whereClause = "id = ?";
-        String[] whereArgs = new String[]{String.valueOf(id)};
-        return DBAgent.deleteRecords(tableName, whereClause, whereArgs);
-
+        List<String> whereArgs = Collections.singletonList(String.valueOf(id));
+        try {
+            return DBAgent.deleteRecords(tableName, whereClause, whereArgs);
+        } catch (SQLException e) {
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[ProgramHandler.deleteRecord]" : e.getMessage());
+        }
+        return -1;
     }
 }
