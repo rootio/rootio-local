c46a8e21a607dac22651ca7a9cdd2351ad730cc6
Jude Love <jude17love@hotmail.com>
2020-07-24 14:34:39 +0100

    Adadpte StationHandler class

--- src/main/java/org/rootio/services/synchronization/StationHandler.java
+++ src/main/java/org/rootio/services/synchronization/StationHandler.java
@@ -1,27 +1,18 @@
-/**
- *
- */
 package org.rootio.services.synchronization;
 
+import org.json.JSONException;
 import org.json.JSONObject;
-import org.rootio.handset.BuildConfig;
-import org.rootio.tools.cloud.Cloud;
-import org.rootio.tools.utils.Utils;
+import org.rootio.configuration.Configuration;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
+import java.util.Iterator;
 
 /**
  * @author Jude Mukundane, M-ITI/IST-UL
  */
 public class StationHandler implements SynchronizationHandler {
-    private Context parent;
-    private Cloud cloud;
 
-    public StationHandler(Context parent, Cloud cloud) {
-        this.parent = parent;
-        this.cloud = cloud;
+    public StationHandler() {
+
     }
 
     public JSONObject getSynchronizationData() {
@@ -34,39 +25,39 @@ public class StationHandler implements SynchronizationHandler {
      * @param synchronizationResponse The response returned from the cloud server
      */
     public void processJSONResponse(JSONObject synchronizationResponse) {
-        boolean isConfigChanged = this.isConfigChanged(synchronizationResponse.toString());
-        //Lazy approach: save all JSON to prefs in one field, get it out and parse it later :-D
-        ContentValues values  = new ContentValues();
-        values.put("station_information", synchronizationResponse.toString());
-        Utils.savePreferences(values, this.parent);
-        if(isConfigChanged)
+        var ref = new Object() {
+            boolean isConfigChanged = false;
+        };
+        Iterator<String> iter = synchronizationResponse.keys();
+        iter.forEachRemaining(entry -> {
+            try {
+                ref.isConfigChanged = ref.isConfigChanged || Configuration.setProperty(entry, synchronizationResponse.getString(iter.next()));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+
+        if(ref.isConfigChanged)
         {
             this.announceSIPChange();
         }
-        //Utils.saveJSONToFile(this.parent, synchronizationResponse, this.parent.getFilesDir().getAbsolutePath() + "/station.json");
     }
 
     @Override
     public String getSynchronizationURL() {
-        return String.format("%s://%s:%s/%s/%s/information?api_key=%s&version=%s_%s", this.cloud.getServerScheme(), this.cloud.getServerAddress(), this.cloud.getHTTPPort(), "api/station", this.cloud.getStationId(), this.cloud.getServerKey(), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
-    }
-
-    private boolean isConfigChanged(String newConfiguration)
-    {
-        String currentConfiguration = (String) Utils.getPreference("station_information", String.class, this.parent);
-        return (currentConfiguration != null && !currentConfiguration.equals(newConfiguration));
+        return String.format("%s://%s:%s/%s/%s/information?api_key=%s&version=%s_%s", Configuration.getProperty("server_scheme"), Configuration.getProperty("server_address"), Configuration.getProperty("http_port"), "api/station", Configuration.getProperty("station_id"), Configuration.getProperty("server_key"), Configuration.getProperty("build_version"), Configuration.getProperty("build_version"));
     }
 
     private void announceSIPChange()
     {
-        //This is lazy -- any station change will result in re-registration. Ideal is to extract SIP components and compare them
-        try {
-               Intent intent = new Intent("org.rootio.handset.SIP.CONFIGURATION_CHANGE");
-                this.parent.sendBroadcast(intent);
-        }
-        catch(Exception ex){
-            //todo: log this
-        }
+//        //This is lazy -- any station change will result in re-registration. Ideal is to extract SIP components and compare them
+//        try {
+//               Intent intent = new Intent("org.rootio.handset.SIP.CONFIGURATION_CHANGE");
+//                this.parent.sendBroadcast(intent);
+//        }
+//        catch(Exception ex){
+//            //todo: log this
+//        }
     }
 
 }
