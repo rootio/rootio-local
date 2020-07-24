3a1c5140122e4d11d89f2c0cba3fa4cc8285c465
Jude Love <jude17love@hotmail.com>
2020-07-24 19:27:16 +0100

    Adapted SMSReceiver

--- src/main/java/org/rootio/tools/sms/SMSReceiver.java
+++ src/main/java/org/rootio/tools/sms/SMSReceiver.java
@@ -1,27 +1,12 @@
 package org.rootio.tools.sms;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.provider.Telephony;
-import android.telephony.SmsMessage;
-import android.util.Log;
-
-import org.rootio.handset.R;
+import org.rootio.tools.ipc.BroadcastReceiver;
 
 public class SMSReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        try {
-            if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
-                for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
-                    new SMSSwitch(context, smsMessage).getMessageProcessor().ProcessMessage();
-                }
-            }
-        } catch (Exception ex) {
-            Log.e(context.getString(R.string.app_name), ex.getMessage() == null ? "Null pointer exception(SMSReceiver.onreceive)" : ex.getMessage());
-        }
+    public void onReceive(Object message) {
+        new SMSSwitch(message).getMessageProcessor().ProcessMessage();
     }
 
 
