c541639f1fd0ec9b68995bd3ca26e1af5cc9e89f
Jude Love <jude17love@hotmail.com>
2020-07-24 19:34:53 +0100

    Adapted CallAuthenticator

--- src/main/java/org/rootio/tools/telephony/CallAuthenticator.java
+++ src/main/java/org/rootio/tools/telephony/CallAuthenticator.java
@@ -1,32 +1,23 @@
 package org.rootio.tools.telephony;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.HashSet;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.rootio.handset.R;
-import org.rootio.tools.utils.Utils;
 
-import android.content.Context;
-import android.util.Log;
+import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CallAuthenticator {
 
-    private Context parent;
     private JSONObject whiteList;
 
-    public CallAuthenticator(Context parent) {
-        this.parent = parent;
+    public CallAuthenticator() {
         try {
-            this.whiteList = new JSONObject((String) Utils.getPreference("whitelist", String.class, this.parent));
+            this.whiteList = new JSONObject(); //(String) Utils.getPreference("whitelist", String.class, this.parent));
         } catch (Exception e) {
-            Log.e(this.parent.getString(R.string.app_name), e.getMessage() == null ? "NullPointerException(CallAuthenticator())" : e.getMessage());
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[CallAuthenticator.CallAuthenticator]" : e.getMessage());
         }
     }
-
 
     public boolean isWhiteListed(String phoneNumber) {
         HashSet<String> pool = new HashSet<>();
@@ -42,10 +33,10 @@ public class CallAuthenticator {
             }
            return this.whiteList.getJSONArray("whitelist").toString().contains(sanitizedPhoneNumber); // potentially
             // problematic
-        } catch (JSONException ex) {
-            Log.e(this.parent.getString(R.string.app_name), ex.getMessage() == null ? "NullPointerException(CallAuthenticator.isWhiteListed)" : ex.getMessage());
-        } catch (NullPointerException ex) {
-            Log.e(this.parent.getString(R.string.app_name), ex.getMessage() == null ? "NullPointerException(CallAuthenticator.isWhiteListed)" : ex.getMessage());
+        } catch (JSONException e) {
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[CallAuthenticator.isWhiteListed]" : e.getMessage());
+        } catch (NullPointerException e) {
+            Logger.getLogger("RootIO").log(Level.WARNING, e.getMessage() == null ? "Null pointer[CallAuthenticator.isWhiteListed]" : e.getMessage());
         }
         return false;
     }
