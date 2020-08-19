d89ef6daa241f12c60b9dc2523d53f198a661de3
Jude Love <jude17love@hotmail.com>
2020-08-19 12:01:19 +0100

    Added Media player based on Java standard lib

---
+++ src/main/java/org/rootio/tools/media/MediaPlayer.java
@@ -0,0 +1,134 @@
+package org.rootio.tools.media;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class MediaPlayer {
+    private Runnable errorHandler, endHandler, readyHandler;
+    private String filename;
+    private SourceDataLine line;
+    private Status status;
+
+    MediaPlayer(String filename)
+    {
+        this.filename = filename;
+    }
+
+    public void play() {
+        AudioInputStream din = null;
+        AudioInputStream in = null;
+        try {
+            File file = new File(filename);
+            in = AudioSystem.getAudioInputStream(file);
+            AudioFormat baseFormat = in.getFormat();
+            AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                    baseFormat.getSampleRate(),
+                    16,
+                    baseFormat.getChannels(),
+                    baseFormat.getChannels() * 2,
+                    baseFormat.getSampleRate(),
+                    false);
+            din = AudioSystem.getAudioInputStream(decodedFormat, in);
+            status = Status.PLAYING;
+            rawplay(decodedFormat, din);
+            status = Status.STOPPED;
+            if(endHandler != null) {
+                new Thread(endHandler).start();
+            }
+        } catch (Exception e) {
+            status = Status.STOPPED;
+            if(errorHandler != null) {
+                new Thread(errorHandler).start();
+            }
+        }
+        finally {
+            try
+            {
+                in.close();
+                din.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setOnError(Runnable r)
+    {
+            this.errorHandler = r;
+    }
+
+    public void setOnEnd(Runnable r)
+    {
+            this.endHandler = r;
+    }
+
+    private void rawplay(AudioFormat targetFormat, AudioInputStream din) throws IOException, LineUnavailableException {
+        byte[] data = new byte[4096];
+        line = getLine(targetFormat);
+        if (line != null) {
+            // Start
+            line.start();
+            //call the readiness handler
+            if(readyHandler != null)
+            {
+                new Thread(readyHandler).start();
+            }
+            int nBytesRead = 0, nBytesWritten = 0;
+            while (nBytesRead != -1) {
+                nBytesRead = din.read(data, 0, data.length);
+                if (nBytesRead != -1) nBytesWritten = line.write(data, 0, nBytesRead);
+            }
+            stop();
+            din.close();
+        }
+    }
+
+    public void setVolume(float volume) {
+        if (line.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+            FloatControl vol = (FloatControl) line.getControl(FloatControl.Type.MASTER_GAIN);
+            vol.setValue(vol.getValue() * volume);
+        }
+    }
+
+    public void stop()
+    {
+        status = Status.STOPPED;
+        try {
+            line.drain();
+            line.stop();
+            line.close();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    private SourceDataLine getLine(AudioFormat audioFormat) throws LineUnavailableException {
+        SourceDataLine res = null;
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+        res = (SourceDataLine) AudioSystem.getLine(info);
+        res.open(audioFormat);
+        return res;
+    }
+
+    public  Status getStatus() {
+        return status;
+    }
+
+    public void setOnReady(Runnable r) {
+        readyHandler = r;
+    }
+
+    public float getVolume() {
+        if (line.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+            FloatControl vol = (FloatControl) line.getControl(FloatControl.Type.MASTER_GAIN);
+            return vol.getValue();
+        }
+        return 0;
+    }
+
+    enum Status {PLAYING, PAUSED, STOPPED};
+}
+
