2a8a45bfc02a03e8ceff72e2587713b9fc871927
Jude Love <jude17love@hotmail.com>
2020-08-21 14:02:36 +0100

    Streamlining SMS switch to work with new Message class

--- src/main/java/org/rootio/tools/sms/SMSSwitch.java
+++ src/main/java/org/rootio/tools/sms/SMSSwitch.java
@@ -1,34 +1,23 @@
 package org.rootio.tools.sms;
 
+import org.rootio.messaging.Message;
+
 public class SMSSwitch {
 
-    private String[] messageParts;
-    private String from;
+    private static String from;
 
-    public SMSSwitch(Object message) {
-//        this.parent = parent;
-//        this.from = message.getOriginatingAddress();
-//        this.messageParts = this.getMessageParts(message.getMessageBody().toLowerCase());
+    public static void switchSMS(Message message) {
+        String fr = (String)message.getPayLoad().get("from");
+        if(fr != null) {
+            from = fr; //a header. only set the from
+        }
+        String body = (String)message.getPayLoad().get("body");
+        if(body != null) { //a body. process this.
+            String[] messageParts = ((String) message.getPayLoad().get("body")).split("|");
+            switchSMS(messageParts).ProcessMessage();
+        }
     }
 
-    /**
-     * Gets a Message processor to be used to process the received message
-     *
-     * @return A MessageProcessor object to process the message
-     */
-    public MessageProcessor getMessageProcessor() {
-        return this.switchSMS(this.messageParts);
-    }
-
-    /**
-     * Tokenizes the message into parts that can be analyzed for actions
-     *
-     * @param message The message to be broken down
-     * @return Array of strings representing tokens in the message
-     */
-    private String[] getMessageParts(String message) {
-        return message.split("[|]");
-     }
 
     /**
      * Examines the message parts and returns a suitable message processor to
@@ -37,26 +26,26 @@ public class SMSSwitch {
      * @param messageParts Tokens from the message to be analyzed
      * @return a MessageProcessor to process the message
      */
-    private MessageProcessor switchSMS(String[] messageParts) {
+    private static MessageProcessor switchSMS(String[] messageParts) {
         String keyword = messageParts.length > 0 ? messageParts[0] : "";
         switch(keyword.toLowerCase())
         {
             case "network": //network|<wifi | gsm >|<on | off | status>
                 return new NetworkSMSHandler(from, messageParts);
-            case "station":
+            case "station": //station|
                 return new StationSMSHandler(from, messageParts);
             case "services": //services|<service_id>|<start | stop | status>
                 return new ServicesSMSHandler(from, messageParts);
-            case "resources":
+            case "resources": //resources
                 return new ResourcesSMSHandler(from, messageParts);
-            case"sound":
+            case"sound": //sound
                 return new SoundSMSHandler(from, messageParts);
-            case "whitelist":
+            case "whitelist": //whitelist
                 return new WhiteListSMSHandler(from, messageParts);
             case "ussd": //ussd|*123#
-                return new USSDSMSHandler(this.from, messageParts);
-            case "mark":
-                return new MarkHandler(this.from, messageParts);
+                return new USSDSMSHandler(from, messageParts);
+            case "mark": //mark|<...>
+                return new MarkHandler(from, messageParts);
             default:
             return null;
         }
